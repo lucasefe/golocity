93dfc4d4c6439f4ed6f0301106723aa79e08847e
Lucas Florio <lucasefe@gmail.com>
2017-03-03 13:25:17 -0300

    Cleanup

--- main.go
+++ main.go
@@ -8,18 +8,33 @@ import (
 	"github.com/lucasefe/go-nv/ui"
 )
 
-const (
-	// SearchView is the name of the Search view
-	SearchView = "SearchInput"
-	// StatusView is the name of the Status view
-	StatusView = "Status"
-)
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-var vbuf, buf string
+	g.Cursor = true
+	g.Mouse = false
+
+	g.SetManagerFunc(ui.RenderLayout)
+
+	if err := g.SetKeybinding(ui.SearchView, gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding(ui.SearchView, gocui.KeyEnter, gocui.ModNone, search); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+
+	g.Close()
+}
 
 func quit(g *gocui.Gui, v *gocui.View) error {
-	vbuf = v.ViewBuffer()
-	buf = v.Buffer()
 	return gocui.ErrQuit
 }
 
@@ -29,7 +44,7 @@ func search(g *gocui.Gui, v *gocui.View) error {
 	v.SetCursor(0, 0)
 
 	g.Execute(func(g *gocui.Gui) error {
-		status, err := g.View(StatusView)
+		status, err := g.View(ui.StatusView)
 		if err != nil {
 			return err
 		}
@@ -40,42 +55,4 @@ func search(g *gocui.Gui, v *gocui.View) error {
 	})
 
 	return nil
-}
-
-func layout(g *gocui.Gui) error {
-	if err := ui.RenderSearch(g, SearchView); err != nil {
-		return err
-	}
-
-	if err := ui.RenderStatus(g, StatusView); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	g.Cursor = true
-	g.Mouse = true
-
-	g.SetManagerFunc(layout)
-
-	if err := g.SetKeybinding(SearchView, gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding(SearchView, gocui.KeyEnter, gocui.ModNone, search); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-
-	g.Close()
 }
--- ui/render.go
+++ ui/render.go
@@ -2,6 +2,27 @@ package ui
 
 import "github.com/jroimartin/gocui"
 
+const (
+	// SearchView is the name of the Search view
+	SearchView = "SearchInput"
+	// StatusView is the name of the Status view
+	StatusView = "Status"
+)
+
+// RenderLayout ....
+func RenderLayout(g *gocui.Gui) error {
+	if err := RenderSearch(g, SearchView); err != nil {
+		return err
+	}
+
+	if err := RenderStatus(g, StatusView); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RenderSearch ..
 func RenderSearch(g *gocui.Gui, name string) error {
 	maxX, _ := g.Size()
 
@@ -19,6 +40,7 @@ func RenderSearch(g *gocui.Gui, name string) error {
 	return nil
 }
 
+// RenderStatus ...
 func RenderStatus(g *gocui.Gui, name string) error {
 	maxX, maxY := g.Size()
 	if v, err := g.SetView(name, 0, maxY-3, maxX-1, maxY-1); err != nil {
